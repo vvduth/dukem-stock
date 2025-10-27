f19198389e5bb2fd388824505f542a7d0424725a
vvduth <ducthai060501@gmail.com>
2025-10-27 23:43:58 +0200

    feat(database): add mongoose connection utility and caching mechanism

---
+++ database/mongoose.ts
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose';
+const MONGODB_URI = process.env.MONGODB_URI ;
+
+declare global {
+    var mongooseCache :{
+        conn: typeof mongoose | null;
+        promise: Promise<typeof mongoose> | null;
+    }
+}
+
+let cached = global.mongooseCache;
+
+if (!cached) {
+    cached = global.mongooseCache = { conn: null, promise: null };
+}
+
+export const connectToDatabase = async () => {
+    if (!MONGODB_URI ) {
+        throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
+    }
+
+    if (cached.conn) {
+        return cached.conn;
+    }
+
+    if (!cached.promise) {
+        cached.promise = mongoose.connect(MONGODB_URI ,{bufferCommands: false})
+    }
+
+    try {
+        cached.conn = await cached.promise;
+    } catch (error) {
+        cached.promise = null;
+        throw error;
+    }
+
+    console.log('MongoDB connected ', process.env.NODE_ENV, " ", MONGODB_URI );
+}
